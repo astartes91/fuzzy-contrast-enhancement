bc3bac5bd5a1361a7ee44c20c9149436d6cc8e73
astartes91 <astartes91@gmail.com>
2015-02-16 12:21:26 +0600

    Converting to Grayscale image added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,13 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using Color = System.Drawing.Color;
 
 namespace FuzzyContrastEnhancement
 {
@@ -22,6 +25,8 @@ namespace FuzzyContrastEnhancement
     /// </summary>
     public partial class MainWindow : Window
     {
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
         public MainWindow()
         {
             InitializeComponent();
@@ -31,6 +36,74 @@ namespace FuzzyContrastEnhancement
         {
             OpenFileDialog dialog = new OpenFileDialog();
             bool? result = dialog.ShowDialog();
+
+            BitmapImage sourceBitmapImage = new BitmapImage(new Uri(dialog.FileName));
+            SourceImage.Source = sourceBitmapImage;
+
+            BitmapImage grayBitmapImage = GetGrayscaleImage(sourceBitmapImage);
+            GrayImage.Source = grayBitmapImage;
+        }
+
+        private BitmapImage GetGrayscaleImage(BitmapImage source)
+        {
+            Bitmap sourceBitmap = null;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(source));
+                enc.Save(outStream);
+                sourceBitmap = new Bitmap(outStream);
+            }
+
+            Bitmap grayscaleBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height);
+            for (int i = 0; i < sourceBitmap.Width; i++)
+            {
+                for (int j = 0; j < sourceBitmap.Height; j++)
+                {
+                    //get the pixel from the original image
+                    Color originalColor = sourceBitmap.GetPixel(i, j);
+
+                    //create the grayscale version of the pixel
+                    int grayScale = (int)((originalColor.R * .3) + (originalColor.G * .59)
+                        + (originalColor.B * .11));
+
+                    //create the color object
+                    Color newColor = Color.FromArgb(grayScale, grayScale, grayScale);
+
+                    //set the new image's pixel to the grayscale version
+                    grayscaleBitmap.SetPixel(i, j, newColor);
+                }
+            }
+
+            IntPtr hBitmap = grayscaleBitmap.GetHbitmap();
+            BitmapImage grayScaleBitmapImage = new BitmapImage();
+
+            try
+            {
+                BitmapSource grayScaleBitmapSource = Imaging.CreateBitmapSourceFromHBitmap(
+                             hBitmap,
+                             IntPtr.Zero,
+                             Int32Rect.Empty,
+                             BitmapSizeOptions.FromEmptyOptions());
+
+                BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+                MemoryStream memoryStream = new MemoryStream();
+
+                encoder.Frames.Add(BitmapFrame.Create(grayScaleBitmapSource));
+                encoder.Save(memoryStream);
+
+                grayScaleBitmapImage.BeginInit();
+                grayScaleBitmapImage.StreamSource = new MemoryStream(memoryStream.ToArray());
+                grayScaleBitmapImage.EndInit();
+
+                memoryStream.Close();
+            }
+            finally
+            {
+                DeleteObject(hBitmap);
+            }
+
+            return grayScaleBitmapImage;
         }
     }
 }
